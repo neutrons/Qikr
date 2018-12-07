1d99bbadb83970485d42fe7afc8337a7d2c71c78
Mathieu Doucet <doucetm@ornl.gov>
2018-12-07 09:52:50 -0500

    working sample and detector

---
+++ qikr.py
@@ -0,0 +1,113 @@
+import numpy as np
+import mcvine, mcvine.components
+from mcni.utils import conversion
+
+def qikr(wl_0=1., center_wl=13.05):
+    """
+        @param wl_min: wavelength at the lower end of the bandwidth
+    """
+    running_length = 0
+    k0 = 2*np.pi/wl_0
+    e0_max = conversion.k2e(k0)
+    kmin = 2*np.pi/40.0
+    e0_min = conversion.k2e(kmin)
+    instrument = mcvine.instrument()
+
+    # Source - includes straight tube
+    beam_width = 0.0266
+    tube_length = 1.0
+    mod = mcvine.components.sources.SNS_source_r1('moderator',
+                                                  S_filename='./SNS-STS-ROT1-15_sp.dat',
+                                                  width=beam_width, height=0.0266, dist=tube_length,
+                                                  xw=beam_width, yh=0.0327, Emin=0, Emax=e0_max)
+    instrument.append(mod, position=(0,0,0))
+    #running_length += 0.001
+
+    # Source Tube  ##################################################################
+    #tube = mcvine.components.optics.Guide_channeled(name='source_tube',
+    #                                                w1=beam_width, h1=0.0266, w2=beam_width, h2=0.0327,
+    #                                                l=tube_length, mx=0, my=0)
+    running_length += tube_length + 0.0001
+
+    # Ballistic Guide 1 #############################################################
+    # From 100 cm to 620 cm
+    guide_1_length = 5.2
+    guide1 = mcvine.components.optics.Guide_channeled(name='guide_1',
+                                                      w1=beam_width, h1=0.0327, w2=beam_width, h2=0.0642,
+                                                      l=guide_1_length, mx=5, my=5)
+    instrument.append(guide1, position=(0,0,running_length))
+    print ("Ballistic guide 1: %s m" % running_length)
+    running_length += guide_1_length + 0.0001
+
+    # T0 chopper ###################################################################
+    # From 620 cm to 664.8 cm
+    #t0_chopper = mcvine.components.optics.Vertical_T0(name='t0chopper', len=0.448,
+    #                                                  w1=beam_width, w2=beam_width, nu=120, delta=0.0, tc=0., ymin=-.045, ymax=0.045)
+    #instrument.append(t0_chopper, position=(0,0,6.2))
+    running_length += 0.448
+
+    # Ballistic Guide 2 #############################################################
+    # From 664.8 cm to 714.8 cm
+    guide_2_length = 0.5
+    guide2 = mcvine.components.optics.Guide_channeled(name='guide_2',
+                                                      w1=beam_width, h1=0.067, w2=beam_width, h2=0.07,
+                                                      l=guide_2_length, mx=5, my=5)
+    instrument.append(guide2, position=(0,0,running_length))
+    print ("Ballistic guide 2: %s m" % running_length)
+    running_length += guide_2_length + 0.0001
+
+    
+    # Bandwidth chopper ############################################################
+    # From 714.8 cm to 722.1 cm
+    instrument.append(mcvine.components.monitors.NeutronToStorage('save_pre', 'beam_pre_chopper.neutrons'),
+                      position=(0,0,running_length))
+    running_length += 0.0001
+    b_chopper_length = 0.073
+    b_chopper_dist = running_length + b_chopper_length/2.0  # center of chopper
+    print ("Bandwidth chopper: %s m" % b_chopper_dist)
+
+    aperture_bottom = 0.2689
+    aperture_top = 0.33949
+    radius_to_beam = 0.30390
+    open_angle = 111.964
+
+    kmid = 2*np.pi/center_wl
+    v = conversion.k2v(kmid)
+    delay = b_chopper_dist/v
+    disk_chopper = mcvine.components.optics.DiskChopper_v2(name='band_chopper', 
+                                                           yheight=aperture_top-aperture_bottom,
+                                                           nslit=1,
+                                                           radius=aperture_top,
+                                                           theta_0=open_angle,
+                                                           delay=delay, nu=7.5)
+    running_length += 0.0001
+    instrument.append(disk_chopper, position=(0,0, b_chopper_dist))
+    instrument.append(mcvine.components.monitors.NeutronToStorage('save_post', 'beam_post_chopper.neutrons'),
+                      position=(0,0,running_length))
+    running_length += b_chopper_length + 0.0001
+
+
+    # Ballistic Guide 3 #############################################################
+    # From 722.1 cm to 1264 cm
+    guide_3_length = 5.419
+    guide3 = mcvine.components.optics.Guide_channeled(name='guide_3',
+                                                      w1=beam_width, h1=0.07, w2=beam_width, h2=0.1033,
+                                                      l=guide_3_length, mx=5, my=5)
+    instrument.append(guide3, position=(0,0,running_length))
+    running_length += guide_3_length + 0.0001
+
+
+    # Tapered guide #################################################################
+    # From 1264 cm to 1599 cm
+    guide_4_length = 3.35
+    guide4 = mcvine.components.optics.Guide_channeled(name='guide_4',
+                                                      w1=beam_width, h1=0.1033, w2=beam_width, h2=0.02,
+                                                      l=guide_4_length, mx=5, my=5)
+    instrument.append(guide4, position=(0,0,running_length))
+    running_length += guide_4_length + 0.0001
+
+    instrument.append(mcvine.components.monitors.NeutronToStorage('save', 'beam.neutrons'),
+                      position=(0,0,running_length))
+    running_length += 0.0001
+
+    return instrument
--- sample.py
+++ sample.py
@@ -1,9 +1,12 @@
+import numpy as np
 import mcvine, mcvine.components
 from mcni.AbstractComponent import AbstractComponent
 from mcni.utils import conversion
-import numpy as np
-import os
 from mcni import neutron_buffer, neutron
+from mcni.neutron_storage import neutrons_as_npyarr, ndblsperneutron
+
+DEBUG = False
+
 
 class Sample(AbstractComponent):
 
@@ -18,9 +21,10 @@ class Sample(AbstractComponent):
     def process(self, neutrons): # input neutron is buffer
         if not len(neutrons):
             return
-        from mcni.neutron_storage import neutrons_as_npyarr, ndblsperneutron # number of doubles per neutrons thats means each neutron is represented by x, y, z, vx, vy, vz, s1, s2, t, t0, p (10 double variables)
 
-        arr = neutrons_as_npyarr(neutrons) #converting the input neutrons to array
+        # Number of doubles per neutrons thats means each neutron is represented
+        # by x, y, z, vx, vy, vz, s1, s2, t, t0, p (10 double variables)
+        arr = neutrons_as_npyarr(neutrons)
         arr.shape = -1, ndblsperneutron
         x = arr[:, 0];
         y = arr[:, 1];
@@ -28,31 +32,52 @@ class Sample(AbstractComponent):
         vx = arr[:, 3];
         vy = arr[:, 4];
         vz = arr[:, 5]
-        s1 = arr[:, 6];
-        s2 = arr[:, 7];
         t = arr[:, 8];
-        t0 = t.copy()
         p = arr[:, 9]
 
-        # propagate to y = 0
+        # Propagate to y = 0
         self._propagateToY0(x, y, z, vx, vy, vz, t)
 
         # Apply filter if area is positive
         assert self.xwidth > 0 and self.zheight > 0
 
         # Filter
-        ftr = (x >= -self.xwidth / 2) * (x <= self.xwidth / 2) * (z >= -self.zheight / 2) * (z <= self.zheight / 2) * (
-                    t > t0)
+        do_filter = True
+        ftr = (x >= -self.xwidth / 2) * (x <= self.xwidth / 2) * (y >= -self.zheight / 2) * (y <= self.zheight / 2)
 
-        # reflection
-        vy[ftr] *= -1
+        # Reflection
+        speed = np.sqrt(vx**2+vy**2+vz**2)
+        if do_filter:
+            vy[ftr] *= -1
+            q = 2.0*conversion.V2K*speed[ftr] * vy[ftr]/vz[ftr]
+            p[ftr] *= self.reflectivity(self.rq, q)
+        else:
+            vy *= -1
+            q = 2.0*conversion.V2K*speed * vy/vz
+            p *= self.reflectivity(self.rq, q)
 
-        # adjust probability
-        Q = np.abs(conversion.V2K * 2 * vy[ftr])
-        p[ftr] *= self.reflectivity(self.rq, Q)
+        if DEBUG:
+            for i in range(len(arr)):
+                self.print_neutron(arr[i], q[i])
 
         neutrons.from_npyarr(arr)
         return  neutrons
+
+    def print_neutron(self, n, q0=0.0):
+        tof = n[8]
+        vx = n[3]
+        vy = n[4]
+        vz = n[5]
+        h = 6.626e-34  # m^2 kg s^-1
+        m = 1.675e-27  # kg
+        dist = tof * vz
+        cst = dist / h * m
+        wl  = tof / cst * 1e10
+        k = conversion.V2K*vz
+        wl2 = 2.0 * np.pi / k
+        q = 4.0*np.pi/wl * vy/vz
+        theta = np.arcsin(vy/vz) * 180.0 / np.pi
+        print "z = %s    wl = %s (%s)   q = %s (%s)   theta = %s" % (dist, wl, wl2, q, q0, theta)
 
     def _propagateToY0(self, x, y, z, vx, vy, vz, t):
         dt = -y / vy
@@ -65,5 +90,3 @@ class Sample(AbstractComponent):
     def reflectivity(self, rq, *args, **kwargs):
         R = rq(*args, **kwargs)
         return R
-
-
